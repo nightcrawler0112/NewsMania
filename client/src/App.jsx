bda2a1f38986460089c8f910db34eeb3f2c5e185
Navin <rajnavinraj23@gmail.com>
2024-06-26 13:41:35 +0530

    Completed the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header' 
-import { BrowserRouter as Router, Route, BrowserRouter} from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import AllNews from "./components/AllNews";
+// import Footer from "./components/Footer";
+import TopHeadlines from "./components/TopHeadlines";
 
-const App = () => {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CountryNews from "./components/CountryNews";
 
-  const [count,setCount] = useState(0);
-
+function App() {
+  const [count, setCount] = useState(0)
   return (
-    <>
-    <div className='w-full'>
+    <div className="w-full">
       <BrowserRouter>
-        <Header/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AllNews />} />
+          <Route path="/top-headlines/:category" element={<TopHeadlines />} />
+          <Route path="/country/:iso" element={<CountryNews />} />
+        </Routes>
+        {/* <Cards />  */}
+        {/* <Footer />   */}
       </BrowserRouter>
     </div>
-      
-    </>
-  )
+  );
 }
 
 export default App;
